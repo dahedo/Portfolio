0fe15bad05e5185add738923657b846da8215ecd
daniel_ahedo <daniel.ahedo@alumnos.unican.es>
2020-07-09 23:39:22 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MenuBar from './Components/MenuBar'
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <MenuBar /> 
+    </React.Fragment>
+  );
+}
+
+export default App;
