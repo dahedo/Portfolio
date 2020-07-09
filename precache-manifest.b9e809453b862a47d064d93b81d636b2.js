ed96c1ca84787b1aaeee23867b7eaa050e8639f6
daniel_ahedo <daniel.ahedo@alumnos.unican.es>
2020-07-10 00:06:01 +0200

    Updates

---
+++ precache-manifest.b9e809453b862a47d064d93b81d636b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c100204783daf3b4d827bca310e1e003",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "83b3aecc5fe10c894779",
+    "url": "/Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "856914fa6f0790d53149",
+    "url": "/Portfolio/static/js/2.646bad6d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio/static/js/2.646bad6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83b3aecc5fe10c894779",
+    "url": "/Portfolio/static/js/main.a468e64a.chunk.js"
+  },
+  {
+    "revision": "593d957f82fe3f176214",
+    "url": "/Portfolio/static/js/runtime-main.cec0fe62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
